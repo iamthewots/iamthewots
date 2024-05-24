1c4fc336619aa6243907406f50278b5b49dabc04
iamthewots <thewots.developer@gmail.com>
2024-05-24 22:25:19 +0200

    First commit

--- frontend/framework/src/views/HomeView.vue
+++ frontend/framework/src/views/HomeView.vue
@@ -12,8 +12,8 @@ window.addEventListener("resize", () => {
 </script>
 
 <template>
-  <main class="flex flex-direction-column padding-main gap-y-md">
-    <section id="font-size" class="box grid gap-md">
+  <main class="grid-content-layout padding-main gap-y-md">
+    <section id="font-size" class="box grid gap-y-md">
       <header>
         <h2 class="text-is-title text-color-accent-1">Font sizes</h2>
         <p>Current HTML font size: {{ currentFontSizeInPx }}</p>
@@ -31,53 +31,57 @@ window.addEventListener("resize", () => {
         >
       </p>
     </section>
-    <section id="layouts" class="flex flex-direction-column gap-y-xl">
-      <div
-        id="flex-rabbit-layout"
-        class="box centered-content flex-rabbit-layout box-shadow-groovy gap-md"
-      >
-        <img
-          class="border-circle aspect-ratio-square width-xs"
-          src="https://loremflickr.com/500/500/cat"
-          alt=""
-        />
-        <div>
-          <h3 class="text-color-accent-2">flex-rabbit-layout</h3>
-          <div>
-            <p>by Leonardo Scarfò</p>
-            <i>dual-column</i>
-          </div>
-        </div>
-      </div>
-      <div id="grid-staggered-layout" class="grid-staggered-layout gap-md">
-        <div class="box box-shadow-groovy">
-          <h3 class="text-color-accent-2">grid-staggered-layout</h3>
-          <p>by Leonardo Scarfò</p>
-          <i>Responsive, modular, dual-column</i>
-        </div>
-        <div class="box box-shadow-groovy" v-for="n in 4" :key="n">
-          <p>grid-staggered-layout</p>
-        </div>
-      </div>
-      <div id="grid-content-layout" class="grid-content-layout gap-md">
-        <div class="box">
-          <h3 class="text-color-accent-2">grid-content-layout</h3>
-          <p>by Leonardo Scarfò</p>
-          <i>Responsive, modular, signle-column</i>
-        </div>
-        <div class="_extend-to-full-width box">_extend-to-full-width</div>
-        <div class="_extend-to-gutters box">_extend-to-gutters</div>
-        <div class="_extend-to-left box">_extend-to-left</div>
-        <div class="_extend-to-right box">_extend-to-right</div>
-      </div>
-      <div id="grid-sandwich-layout" class="box centered-content grid-sandwich-layout box-shadow-groovy gap-sm">
-        <h3 class="text-color-accent-2">grid-sandwich-layout</h3>
-        <img src="https://loremflickr.com/300/300/dog" alt="">
+    <div
+      id="flex-rabbit-layout"
+      class="box centered-content flex-rabbit-layout box-shadow-groovy gap-md"
+    >
+      <img
+        class="border-circle aspect-ratio-square width-xs"
+        src="https://loremflickr.com/500/500/cat"
+        alt=""
+      />
+      <div>
+        <h3 class="text-color-accent-2">flex-rabbit-layout</h3>
         <div>
           <p>by Leonardo Scarfò</p>
-          <i>semi-responsive, fixed-layout, signle-column</i>
+          <i>dual-column</i>
         </div>
       </div>
-    </section>
+    </div>
+    <div id="grid-staggered-layout" class="grid-staggered-layout gap-y-md">
+      <div class="box box-shadow-groovy">
+        <h3 class="text-color-accent-2">grid-staggered-layout</h3>
+        <p>by Leonardo Scarfò</p>
+        <i>Responsive, modular, dual-column</i>
+      </div>
+      <div class="box box-shadow-groovy" v-for="n in 4" :key="n">
+        <p>grid-staggered-layout</p>
+      </div>
+    </div>
+    <div
+      id="grid-content-layout"
+      class="_extend-to-full-width grid-content-layout gap-y-md"
+    >
+      <div class="box">
+        <h3 class="text-color-accent-2">grid-content-layout</h3>
+        <p>by Leonardo Scarfò</p>
+        <i>Responsive, modular, signle-column</i>
+      </div>
+      <div class="_extend-to-full-width box">_extend-to-full-width</div>
+      <div class="_extend-to-gutters box">_extend-to-gutters</div>
+      <div class="_extend-to-left box">_extend-to-left</div>
+      <div class="_extend-to-right box">_extend-to-right</div>
+    </div>
+    <div
+      id="grid-sandwich-layout"
+      class="box centered-content grid-sandwich-layout box-shadow-groovy gap-sm"
+    >
+      <h3 class="text-color-accent-2">grid-sandwich-layout</h3>
+      <img src="https://loremflickr.com/300/300/dog" alt="" />
+      <div>
+        <p>by Leonardo Scarfò</p>
+        <i>semi-responsive, fixed-layout, single-column</i>
+      </div>
+    </div>
   </main>
 </template>
