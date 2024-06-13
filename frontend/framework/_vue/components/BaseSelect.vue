d0d837c5c2dc84f067d5068d2a0bd3c9ca305163
iamthewots <thewots.developer@gmail.com>
2024-06-13 11:34:18 +0200

    First commit

--- frontend/framework/_vue/components/BaseSelect.vue
+++ frontend/framework/_vue/components/BaseSelect.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
-export interface BaseSelectProps {}
+export interface BaseSelectProps {
+  multiSelect?: number;
+}
 
 export interface BaseSelectEmits {
-  (e: "update:modelValue", value: string | string[]): void;
+  (e: "update:modelValue", values: Set<string>): void;
+  (e: "base-select:change", values: Set<string>): void;
+  (e: "base-select:select", value: string): void;
+  (e: "base-select:unselect", value: string): void;
 }
 
 export interface BaseSelect {}
@@ -16,78 +21,33 @@ defineOptions({
 
 const props = defineProps<BaseSelectProps>();
 const emits = defineEmits<BaseSelectEmits>();
-const selectedGroupElement = ref<HTMLElement | null>(null);
-const optionsGroupElement = ref<HTMLElement | null>(null);
+const selectedOptionsElement = ref<HTMLElement | null>(null);
+const unselectedOptionsElement = ref<HTMLElement | null>(null);
 const selectedValues = new Set<string>();
 
-// prima ordinare per ordine alfabetico
-// poi ordinare per selezionati o no
-// bingo madaffaka
-
-function handleClickEvent(e: Event) {
+function splitOptions() {
   if (
-    e.target instanceof HTMLOptionElement === false ||
-    selectedGroupElement.value === null ||
-    optionsGroupElement.value === null
+    selectedOptionsElement.value === null ||
+    unselectedOptionsElement.value === null
   ) {
     return;
   }
-
-  const optionElement = e.target;
-  optionElement.classList.remove("base-select--selected");
-  optionElement.classList.remove("base-select--unselected");
-
-  try {
-    selectedGroupElement.value.removeChild(optionElement);
-  } catch (error) {}
-
-  try {
-    optionsGroupElement.value.removeChild(optionElement);
-  } catch (error) {}
-
-  if (selectedValues.has(optionElement.value)) {
-    selectedValues.delete(optionElement.value);
-    optionsGroupElement.value.appendChild(optionElement);
-    optionElement.classList.add("base-select--unselected");
-  } else {
-    selectedValues.add(optionElement.value);
-    selectedGroupElement.value.appendChild(optionElement);
-    optionElement.classList.add("base-select--selected");
-  }
 }
 
-// function handleClickEvent(e: Event) {
-//   if (
-//     e.target instanceof HTMLOptionElement === false ||
-//     wrapperElement.value === null
-//   ) {
-//     return;
-//   }
+function handleClickEvent() {}
 
-//   const { value } = e.target;
-//   e.target.classList.remove("base-select--selected");
-//   wrapperElement.value.removeChild(e.target);
-
-//   if (selectedValues.has(value)) {
-//     wrapperElement.value.appendChild(e.target);
-//     selectedValues.delete(value);
-//   } else {
-//     e.target.classList.add("base-select--selected");
-//     wrapperElement.value.prepend(e.target);
-//     selectedValues.add(value);
-//   }
-// }
+onMounted(() => {});
 </script>
 
 <template>
-  <div class="base-select" @click="handleClickEvent" ref="wrapperElement">
+  <div class="base-select" @click="handleClickEvent" v-bind="$attrs">
     <div
-      class="base-select__group base-select__selected"
-      ref="selectedGroupElement"
+      class="base-select__group base-select__selected-options"
+      ref="selectedOptionsElement"
     ></div>
     <div
-      class="base-select__group base-select__options"
-      ref="optionsGroupElement"
+      class="base-select__group base-select__unselected-options"
+      ref="unselectedOptionsElement"
     >
       <slot></slot>
     </div>
@@ -104,46 +64,31 @@ function handleClickEvent(e: Event) {
   position: relative;
   display: grid;
   border: wtk.get("border");
-  border-radius: wtk.get("border", "radius");
-  padding: wtk.get("spacing", "sm");
-  gap: wtk.get("spacing", "xs");
 
   &__group {
+    justify-content: center;
     flex-wrap: wrap;
     display: flex;
-    gap: wtk.get("spacing", "xs");
+    padding: wtk.get("spacing", "sm");
+    gap: wtk.get("spacing", "sm");
 
     &:empty {
       display: none;
     }
   }
 
-  option {
-    flex-grow: 1;
-    border-radius: inherit;
-    padding: 0.15rem 0.3rem;
-    min-width: min(20%, 100px);
-    text-align: center;
-    background-color: var(--color-input-handle);
-    cursor: pointer;
-
-    &.base-select--selected {
-      animation: zoom-in 200ms ease forwards, fade-in 200ms ease forwards;
-      border-radius: wtk.get("border", "radius");
-      background-color: var(--color-input-active);
-    }
-
-    &.base-select--unselected {
-      animation: zoom-out 200ms ease forwards, fade-in 200ms ease forwards;
-    }
+  &__unselected-options {
+    // max-3-rows, then scroll?
   }
 
-  select {
-    position: absolute;
-    z-index: -1;
-    opacity: 0;
-    width: 0;
-    height: 0;
+  option {
+    flex-grow: 1;
+    border-radius: wtk.get("border", "radius");
+    padding: wtk.get("spacing", 0.15rem) wtk.get("spacing", 0.3rem);
+    min-width: min(20%, 200px);
+    background-color: var(--color-input-handle);
+    text-align: center;
+    cursor: pointer;
   }
 }
 </style>
