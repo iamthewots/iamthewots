9dd0b83b64e8f1ff0a6007e1aa77f3f30f01f12b
iamthewots <thewots.developer@gmail.com>
2024-06-15 11:17:56 +0200

    First commit

--- frontend/framework/_vue/components/BaseButton.vue
+++ frontend/framework/_vue/components/BaseButton.vue
@@ -1,40 +1,5 @@
 <script setup lang="ts"></script>
 
-<template>
-  <button class="base-button" @touchstart=""><slot></slot></button>
-</template>
+<template></template>
 
-<style lang="scss">
-@use "@_sass/modules/fx.scss";
-@use "@_sass/modules/theme.scss";
-@use "@_sass/wtk.scss";
-
-.base-button {
-  @include fx.set-box-shadow("button");
-  @include theme.set-color-scheme("button");
-
-  align-items: center;
-  justify-content: center;
-  transition: all wtk.get("duration", "xs") ease;
-  display: inline-flex;
-  outline: none;
-  border: wtk.get("border");
-  border-radius: wtk.get("border", "radius");
-  padding-inline: wtk.get("spacing", "sm");
-  gap: wtk.get("spacing", "sm");
-  height: wtk.get("height", "input");
-  font-family: wtk.get("font-family", "input");
-  font-size: wtk.get("font-size", "md");
-  cursor: pointer;
-
-  &:disabled {
-    opacity: 0.25;
-  }
-
-  &:active {
-    box-shadow: none;
-    scale: 0.975;
-    translate: 0 0.2rem;;
-  }
-}
-</style>
+<style lang="scss"></style>
