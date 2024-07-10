ebec5b2b99a7e674ab703423c3d745c374f524ae
iamthewots <thewots.developer@gmail.com>
2024-07-10 04:07:32 +0200

    First commit

--- frontend/framework/_lib/pointer-gesture.ts
+++ frontend/framework/_lib/pointer-gesture.ts
@@ -1,6 +1,5 @@
 export interface PointerGestureSettings {
-  minPointers?: number;
-  maxPointers?: number;
+  pointersRequired?: number;
   pointerTypesAllowed?: PointerType[] | "all";
   defaultMouseButton?: MouseButton;
 }
@@ -19,7 +18,7 @@ export interface PointerData {
   pointerId: number;
   type: PointerType;
   buttonsDown: Set<number>;
-  coordinatesHistory: {
+  points: {
     x: number;
     y: number;
     deltaX: number;
@@ -32,18 +31,25 @@ export interface PointerData {
 
 export class PointerGesture {
   _element: HTMLElement;
-  _pointersDataArray: PointerData[] = [];
-  #minPointers: number;
-  #maxPointers: number;
+  _pointersDataMap: Map<number, PointerData> = new Map();
+  #pointersRequired: number;
   #pointerTypesAllowed: PointerType[] | "all";
   #defaultMouseButton: MouseButton;
+  #gestureIsActive = false;
 
   constructor(element: HTMLElement, settings: PointerGestureSettings = {}) {
     this._element = element;
-    this.#minPointers = settings.minPointers ?? 1;
-    this.#maxPointers = settings.maxPointers ?? this.#minPointers;
+    this.#pointersRequired = settings.pointersRequired ?? 1;
     this.#pointerTypesAllowed = settings.pointerTypesAllowed ?? "all";
     this.#defaultMouseButton = settings.defaultMouseButton ?? MouseButton.Main;
+  }
+
+  findPointerDataByIndex(index: number) {
+    if (index < 0 || index > this._pointersDataMap.size) {
+      return;
+    }
+
+    return Array.from(this._pointersDataMap.values())[index];
   }
 
   connect() {
@@ -66,21 +72,117 @@ export class PointerGesture {
     fn("pointerup", this.#handlePointerUpEvent.bind(this));
   }
 
-  #handlePointerDownEvent(e: PointerEvent) {}
+  #handlePointerDownEvent(e: PointerEvent) {
+    if (this.#isPointerTypeValid(e) === false) {
+      return;
+    }
 
-  #handlePointerMoveEvent(e: PointerEvent) {}
+    const pointerData = this._pointersDataMap.get(e.pointerId);
 
-  #handlePointerLeaveEvent(e: PointerEvent) {}
+    if (pointerData !== undefined) {
+      pointerData.buttonsDown.add(e.button);
 
-  #handlePointerUpEvent(e: PointerEvent) {}
+      return;
+    }
 
-  _deletePointerData(e: PointerEvent) {}
+    if (this.#canAddPointer(e) === false) {
+      return;
+    }
 
-  _findPointerDataIndex(e: PointerEvent) {
-    return this._pointersDataArray.findIndex((pointerData) => {
-      return pointerData.pointerId === e.pointerId;
+    this._pointersDataMap.set(e.pointerId, {
+      pointerId: e.pointerId,
+      type: (e.type as PointerType) ?? "unknown",
+      buttonsDown: new Set<number>([e.button]),
+      points: [
+        {
+          x: e.clientX,
+          y: e.clientY,
+          deltaX: 0,
+          deltaY: 0,
+          distance: 0,
+          angle: 0,
+          timestamp: Date.now(),
+        },
+      ],
     });
+
+    if (this._pointersDataMap.size === this.#pointersRequired) {
+      this.#gestureIsActive = true;
+      this._handleGestureStart(e);
+    }
   }
+
+  #handlePointerMoveEvent(e: PointerEvent) {
+    const pointerData = this._pointersDataMap.get(e.pointerId);
+
+    if (pointerData === undefined) {
+      return;
+    }
+
+    const [firstPoint] = pointerData.points;
+    const x = e.clientX;
+    const y = e.clientY;
+    const deltaX = x - firstPoint.x;
+    const deltaY = y - firstPoint.y;
+    const distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+    pointerData.points.push({
+      x,
+      y,
+      deltaX,
+      deltaY,
+      distance,
+      angle,
+      timestamp: Date.now(),
+    });
+
+    this._handleGestureMove(e);
+  }
+
+  #handlePointerLeaveEvent(e: PointerEvent) {
+    if (this.#gestureIsActive === false) {
+      this._pointersDataMap.delete(e.pointerId);
+
+      return;
+    }
+
+    const pointerData = this._pointersDataMap.get(e.pointerId);
+
+    if (pointerData === undefined) {
+      return;
+    }
+
+    this.#gestureIsActive = false;
+    this._handleGestureEnd(e);
+    this._pointersDataMap.clear();
+  }
+
+  #handlePointerUpEvent(e: PointerEvent) {
+    const pointerData = this._pointersDataMap.get(e.pointerId);
+
+    if (pointerData === undefined) {
+      return;
+    }
+
+    pointerData.buttonsDown.delete(e.button);
+
+    if (
+      pointerData.buttonsDown.size === 0 ||
+      (e.type === "mouse" &&
+        pointerData.buttonsDown.has(this.#defaultMouseButton) === false)
+    ) {
+      this.#gestureIsActive = false;
+      this._handleGestureEnd(e);
+      this._pointersDataMap.clear();
+    }
+  }
+
+  _handleGestureStart(e: PointerEvent) {}
+
+  _handleGestureMove(e: PointerEvent) {}
+
+  _handleGestureEnd(e: PointerEvent) {}
 
   #isPointerTypeValid(e: PointerEvent) {
     if (
@@ -89,7 +191,17 @@ export class PointerGesture {
     ) {
       return false;
     } else {
-      return true
+      return true;
     }
+  }
+
+  #canAddPointer(e: PointerEvent) {
+    if (this._pointersDataMap.size >= this.#pointersRequired) {
+      return false;
+    } else if (e.type === "mouse" && e.button !== this.#defaultMouseButton) {
+      return false;
+    }
+
+    return true;
   }
 }
--- frontend/framework/_lib/pointer-swipe-gesture.ts
+++ frontend/framework/_lib/pointer-swipe-gesture.ts
@@ -1,89 +1,85 @@
-import {
-  PointerGesture,
-  type PointerData,
-  type PointerGestureSettings,
-} from "./pointer-gesture";
+// import {
+//   PointerGesture,
+//   type PointerData,
+//   type PointerGestureSettings,
+// } from "./pointer-gesture";
 
-export interface PointerSwipeGestureSettings extends PointerGestureSettings {
-  threshold?: number;
-  maxTimeout?: number;
-}
+// export interface PointerSwipeGestureSettings extends PointerGestureSettings {
+//   threshold?: number;
+//   maxTimeout?: number;
+// }
 
-interface SwipeData {
-  left: number;
-  right: number;
-  up: number;
-  down: number;
-}
+// interface SwipeData {
+//   left: number;
+//   right: number;
+//   up: number;
+//   down: number;
+// }
 
-interface PointerGestureSwipeEvent {
-  swipeData: SwipeData;
-  mainPointerData: PointerData;
-}
+// interface PointerGestureSwipeEvent {
+//   swipeData: SwipeData;
+//   mainPointerData: PointerData;
+// }
 
-export class PointerSwipeGesture extends PointerGesture {
-  #threshold: number;
-  #maxTimeout: number;
+// export class PointerSwipeGesture extends PointerGesture {
+//   #threshold: number;
+//   #maxTimeout: number;
 
-  constructor(
-    element?: HTMLElement,
-    settings: PointerSwipeGestureSettings = {}
-  ) {
-    super(element, settings);
-    this.#threshold = settings.threshold ?? 100;
-    this.#maxTimeout = settings.maxTimeout ?? 1000;
-  }
+//   constructor(
+//     element: HTMLElement,
+//     settings: PointerSwipeGestureSettings = {}
+//   ) {
+//     super(element, settings);
+//     this.#threshold = settings.threshold ?? 100;
+//     this.#maxTimeout = settings.maxTimeout ?? 1000;
+//   }
 
-  _handleGestureEnd(e: PointerEvent): void {
-    if (this._mainPointerData === undefined || this.#wasSwipeTooSlow()) {
-      return;
-    }
+//   _handleGestureEnd(e: PointerEvent): void {
+//     const threshold = this.#threshold;
+//     const swipeData: SwipeData = {
+//       left: 1,
+//       right: 1,
+//       up: 1,
+//       down: 1,
+//     };
 
-    const threshold = this.#threshold;
-    const swipeData: SwipeData = {
-      left: 1,
-      right: 1,
-      up: 1,
-      down: 1,
-    };
+//     Array.from(this._pointersDataMap.values()).forEach((pointerData) => {
+//       const [lastPoint] = pointerData.points.slice(-1);
+//       swipeData.left *= lastPoint.deltaX < threshold * -1 ? 1 : 0;
+//       swipeData.right *= lastPoint.deltaX > threshold ? 1 : 0;
+//       swipeData.up *= lastPoint.deltaX < threshold * -1 ? 1 : 0;
+//       swipeData.down *= lastPoint.deltaY > threshold ? 1 : 0;
+//     });
 
-    Array.from(this._pointersDataMap.values()).forEach((pointerData) => {
-      const [lastPoint] = pointerData.coordinatesHistory.slice(-1);
-      swipeData.left *= lastPoint.distanceX < threshold * -1 ? 1 : 0;
-      swipeData.right *= lastPoint.distanceX > threshold ? 1 : 0;
-      swipeData.up *= lastPoint.distanceX < threshold * -1 ? 1 : 0;
-      swipeData.down *= lastPoint.distanceY > threshold ? 1 : 0;
-    });
+//     let key: keyof SwipeData;
+//     for (key in swipeData) {
+//       const value = swipeData[key];
 
-    let key: keyof SwipeData;
-    for (key in swipeData) {
-      const value = swipeData[key];
+//       if (value) {
+//         this._element.dispatchEvent(new Event(`pointer-gesture-swipe-${key}`));
+//       }
+//     }
 
-      if (value) {
-        this._element.dispatchEvent(new Event(`pointer-gesture-swipe-${key}`));
-      }
-    }
+//     const event = new CustomEvent<PointerGestureSwipeEvent>(
+//       "pointer-gesture-swipe",
+//       {
+//         detail: {
+//           swipeData,
+//           mainPointerData: this._mainPointerData,
+//         },
+//       }
+//     );
+//     this._element.dispatchEvent(event);
+//   }
 
-    const event = new CustomEvent<PointerGestureSwipeEvent>(
-      "pointer-gesture-swipe",
-      {
-        detail: {
-          swipeData,
-          mainPointerData: this._mainPointerData,
-        },
-      }
-    );
-    this._element.dispatchEvent(event);
-  }
+//   #wasSwipeTooSlow() {
+//     if (this._mainPointerData === undefined) {
+//       return true;
+//     }
 
-  #wasSwipeTooSlow() {
-    if (this._mainPointerData === undefined) {
-      return true;
-    }
+//     const { coordinatesHistory } = this._mainPointerData;
+//     const startDate = coordinatesHistory[0].timestamp;
 
-    const { coordinatesHistory } = this._mainPointerData;
-    const startDate = coordinatesHistory[0].timestamp;
-
-    return Date.now() - startDate > this.#maxTimeout;
-  }
-}
+//     return Date.now() - startDate > this.#maxTimeout;
+//   }
+// }
